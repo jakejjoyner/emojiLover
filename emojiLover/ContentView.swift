07b5e0980e1ee8a90c2c768d917212f6aabcbd96
Jake Joyner <jakejoyner9@gmail.com>
2024-02-01 23:37:38 -0800

    Initial Commit

---
+++ emojiLover/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  emojiLover
+//
+//  Created by Jake Joyner on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ emojiLover/emojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiLoverApp.swift
+//  emojiLover
+//
+//  Created by Jake Joyner on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct emojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
